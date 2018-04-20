dd8a6a8122a5c75ceab9f47bf0f7b6bb53af887f
kksworks <kksworks@gmail.com>
2018-04-20 19:27:04 +0900

    [model : defconfig] quick build script

---
+++ config/system/gtrack/bizincar-dtg/xquick.build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+BUILD_ARG__CORP=bizincar
+BUILD_ARG__SERVER=bizincar-dtg
+######################################
+# edit plz..
+######################################
+BUILD_ARG__VER=xx.yy
+BUILD_ARG__AUTO_PKG=y
+#BUILD_ARG__AUTO_PKG=n
+BUILD_ARG__SUB=alm1
+#BUILD_ARG__SUB=alm2
+
+###########################################
+# using help
+# 1. build system : ./quick.build.sh system
+# 2. clean system : ./quick.build.sh system clean
+# 3. build gtrack3 : ./quick.build.sh gtrack3
+# 4. clean gtrack3 : ./quick.build.sh gtrack3 clean
+###########################################
+#./xbuild3 system CORP=bizincar SERVER=bizincar-dtg VER=01.01 DTG_SERVER=gtrack_tool DTG_MODEL=choyoung
+
+RUN_CMD="./xbuild3 $1 CORP=${BUILD_ARG__CORP} SERVER=${BUILD_ARG__SERVER} VER=${BUILD_ARG__VER} SUB=${BUILD_ARG__SUB} DTG_SERVER=gtrack_tool DTG_MODEL=choyoung AUTO_PKG=${BUILD_ARG__AUTO_PKG} $2"
+
+echo "-----------------------------------------------------------------------------------------"
+echo "build run..."
+echo "${RUN_CMD}"
+echo "-----------------------------------------------------------------------------------------"
+
+${RUN_CMD}
