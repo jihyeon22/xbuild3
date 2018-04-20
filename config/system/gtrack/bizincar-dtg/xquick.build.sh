d4cab99476941c405efeefda2a58ba257501088d
kksworks <kksworks@gmail.com>
2018-04-20 19:29:15 +0900

    [model : defconfig] quick build fix

--- config/system/gtrack/bizincar-dtg/xquick.build.sh
+++ config/system/gtrack/bizincar-dtg/xquick.build.sh
@@ -8,7 +8,7 @@ BUILD_ARG__SERVER=bizincar-dtg
 BUILD_ARG__VER=xx.yy
 BUILD_ARG__AUTO_PKG=y
 #BUILD_ARG__AUTO_PKG=n
-BUILD_ARG__SUB=alm1
+BUILD_ARG__SUB=
 #BUILD_ARG__SUB=alm2
 
 ###########################################
@@ -19,8 +19,7 @@ BUILD_ARG__SUB=alm1
 # 4. clean gtrack3 : ./quick.build.sh gtrack3 clean
 ###########################################
 #./xbuild3 system CORP=bizincar SERVER=bizincar-dtg VER=01.01 DTG_SERVER=gtrack_tool DTG_MODEL=choyoung
-
-RUN_CMD="./xbuild3 $1 CORP=${BUILD_ARG__CORP} SERVER=${BUILD_ARG__SERVER} VER=${BUILD_ARG__VER} SUB=${BUILD_ARG__SUB} DTG_SERVER=gtrack_tool DTG_MODEL=choyoung AUTO_PKG=${BUILD_ARG__AUTO_PKG} $2"
+RUN_CMD="./xbuild3 $1 CORP=${BUILD_ARG__CORP} SERVER=${BUILD_ARG__SERVER} VER=${BUILD_ARG__VER} DTG_SERVER=gtrack_tool DTG_MODEL=choyoung AUTO_PKG=${BUILD_ARG__AUTO_PKG} $2"
 
 echo "-----------------------------------------------------------------------------------------"
 echo "build run..."
