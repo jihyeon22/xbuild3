110af6bd12d5dabfbcf3b9c0d2bba87f0fc86cd3
kksworks <kksworks@gmail.com>
2018-06-01 16:01:22 +0900

    [model] defconfig

--- config/system/gtrack/alloc2/xquick.build.sh
+++ config/system/gtrack/alloc2/xquick.build.sh
@@ -8,7 +8,7 @@ BUILD_ARG__SERVER=alloc2
 BUILD_ARG__VER=xx.yy
 BUILD_ARG__AUTO_PKG=y
 #BUILD_ARG__AUTO_PKG=n
-BUILD_ARG__SUB=alm1
+
 #BUILD_ARG__SUB=alm2
 
 ###########################################
@@ -19,7 +19,35 @@ BUILD_ARG__SUB=alm1
 # 4. clean gtrack3 : ./quick.build.sh gtrack3 clean
 ###########################################
 
-RUN_CMD="./xbuild3 $1 CORP=${BUILD_ARG__CORP} SERVER=${BUILD_ARG__SERVER} VER=${BUILD_ARG__VER} SUB=${BUILD_ARG__SUB} AUTO_PKG=${BUILD_ARG__AUTO_PKG} $2"
+echo -e "--------------------------------------------------"
+echo -e "BUILD TARGET"
+echo -e "--------------------------------------------------"
+echo -e "1. 중고차 위치관제"
+echo -e "2. 중고차 위치관제 + powersave"
+echo -e "3. BCM / OBD 연동 카쉐어링"
+echo -e "select data > "
+
+read SELECT_BUILD
+case $SELECT_BUILD in
+    1)
+        BUILD_ARG__SUB="alm2"
+        RUN_CMD="./xbuild3 $1 CORP=${BUILD_ARG__CORP} SERVER=${BUILD_ARG__SERVER} VER=${BUILD_ARG__VER} SUB=${BUILD_ARG__SUB} AUTO_PKG=${BUILD_ARG__AUTO_PKG} $2"
+
+    ;;
+    2)
+        BUILD_ARG__SUB="alm2"
+        RUN_CMD="./xbuild3 $1 CORP=${BUILD_ARG__CORP} SERVER=${BUILD_ARG__SERVER} VER=${BUILD_ARG__VER} SUB=${BUILD_ARG__SUB} AUTO_PKG=${BUILD_ARG__AUTO_PKG} NO_USE_NETWORK=y $2"
+    ;;
+    3)
+        BUILD_ARG__SUB="alm1"
+        RUN_CMD="./xbuild3 $1 CORP=${BUILD_ARG__CORP} SERVER=${BUILD_ARG__SERVER} VER=${BUILD_ARG__VER} SUB=${BUILD_ARG__SUB} AUTO_PKG=${BUILD_ARG__AUTO_PKG} $2"
+    ;;
+    *)
+        echo "INVAILD SELECT!!!!!!!!!!!!!!!!!!!!!!! BYE"
+        exit
+    ;;
+esac
+
 
 echo "-----------------------------------------------------------------------------------------"
 echo "build run..."
