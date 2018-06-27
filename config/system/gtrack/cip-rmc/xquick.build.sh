d9de97b8af34e22be7781b04c47bea0241de5984
kksworks <kksworks@gmail.com>
2018-06-27 10:56:22 +0900

    [model defconfig] cip-rmc

---
+++ config/system/gtrack/cip-rmc/xquick.build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+BUILD_ARG__CORP=cip
+BUILD_ARG__SERVER=cip-rmc
+######################################
+# edit plz..
+######################################
+BUILD_ARG__VER=xx.yy
+BUILD_ARG__AUTO_PKG=y
+#BUILD_ARG__AUTO_PKG=n
+
+###########################################
+# using help
+# 1. build system : ./quick.build.sh system
+# 2. clean system : ./quick.build.sh system clean
+# 3. build gtrack3 : ./quick.build.sh gtrack3
+# 4. clean gtrack3 : ./quick.build.sh gtrack3 clean
+###########################################
+
+echo -e "--------------------------------------------------"
+echo -e "BUILD TARGET"
+echo -e "--------------------------------------------------"
+echo -e "1. CIP RMC"
+echo -e "select data > "
+
+read SELECT_BUILD
+case $SELECT_BUILD in
+    1)
+        RUN_CMD="./xbuild3 $1 CORP=${BUILD_ARG__CORP} SERVER=${BUILD_ARG__SERVER} VER=${BUILD_ARG__VER} AUTO_PKG=${BUILD_ARG__AUTO_PKG} $2"
+    ;;
+    *)
+        echo "INVAILD SELECT!!!!!!!!!!!!!!!!!!!!!!! BYE"
+        exit
+    ;;
+esac
+
+
+
+echo "-----------------------------------------------------------------------------------------"
+echo "build run..."
+echo "${RUN_CMD}"
+echo "-----------------------------------------------------------------------------------------"
+
+${RUN_CMD}
